8eff046000e686648500201859407d0e6e7b5714
avantikalal <avantikalal1990@gmail.com>
2020-02-03 21:30:40 -0800

    fix peak2bw imports and saving

--- peak2bw.py
+++ peak2bw.py
@@ -30,8 +30,8 @@ import logging
 
 from claragenomics.io.bedgraphio import df_to_bedGraph
 from claragenomics.io.bigwigio import bedgraph_to_bigwig
+from claragenomics.io.bedio import read_intervals, read_sizes
 
-import pandas as pd
 
 # Set up logging
 log_formatter = logging.Formatter(
@@ -70,19 +70,18 @@ def main():
 
     # Read input files
     _logger.info('Reading input files')
-    peaks = pd.read_csv(args.input, sep='\t', header=None,
-                        usecols=[0, 1, 2], skiprows=args.skip)
-    sizes = pd.read_csv(args.sizes, sep='\t', header=None)
+    peaks = read_intervals(args.input, skip=args.skip)
+    sizes = read_sizes(args.sizes)
 
     # Subset to peaks in sizes file
     # TODO: Move this test into the df_to_bedGraph function
-    peaks_filtered = peaks[peaks[0].isin(sizes[0])].copy()
-    _logger.info('Retaining ' + str(len(peaks_filtered)) + ' of ' +
-                 str(len(peaks)) + ' peaks in given chromosomes.')
+    peaks_filtered = peaks[peaks['chrom'].isin(sizes['chrom'])].copy()
+    _logger.info('Retaining ' + str(len(peaks_filtered)) + ' of ' + str(len(
+        peaks)) + ' peaks in given chromosomes.')
 
     # Add score of 1 for all peaks
     _logger.info('Adding score')
-    peaks_filtered[3] = 1
+    peaks_filtered['score'] = 1
 
     # Set prefix for output files
     if args.prefix is None:
@@ -97,7 +96,8 @@ def main():
 
     # Write bigWig and delete bedGraph
     _logger.info('Writing peaks to bigWig file {}'.format(prefix + '.bw'))
-    bedgraph_to_bigwig(prefix + '.bedGraph', args.sizes, deletebg=True)
+    bedgraph_to_bigwig(prefix + '.bedGraph', args.sizes,
+                       deletebg=True, sort=True)
     _logger.info('Done!')
 
 
