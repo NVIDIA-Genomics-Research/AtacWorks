44e3e369629b21f3e2f794c2c5da9d843fb8a754
avantikalal <avantikalal1990@gmail.com>
2020-02-19 13:21:07 -0800

    update e2e scripts

--- scripts/run_inference.sh
+++ scripts/run_inference.sh
@@ -13,21 +13,23 @@
 echo "Set environment"
 set -e
 
-tutorial_dir=$(readlink -f $(dirname "$0"))
-atacworks=$(readlink -f "$tutorial_dir/..")
+script_dir=$(readlink -f $(dirname "$0"))
 
 function print_help {
-    echo "$atacworks/tutorials/run_training.sh <path-to-model-dir> <path-to-test-folder> <path-to-intervals-folder> <path-to-configs-folder>"
-    echo "<path-to-model-dir>        must contain model.pth.tar file"
+    echo "$atacworks/tutorials/run_training.sh <path-to-model-file> <path-to-test-file>  <path-to-configs-folder> <path-to-chromosome-sizes-file> <out_dir>"
+    echo "<path-to-model-file>           path to .pth.tar file"
     echo " "
-    echo "<path-to-test-folder>      must contain noisy_data folder and single bigWig file"
+    echo "<path-to-test-file>            single bigWig file"
     echo " "
-    echo "<path-to-intervals-folder> must contain file with suffix test_intervals.bed"
+    echo "<path-to-configs-folder>       must contain config_params.yaml and model_structure.yaml"
     echo " "
-    echo "<path-to-configs-folder>   must contain config_params.yaml and model_structure.yaml"
+    echo "<path-to-chromosome-sizes-file> tab separated file with names and sizes of all chromosomes to test on"
+    echo " "
+    echo "<out_dir>                      path to output directory."
 }
 
 
+# Check number of args
 if [ $# -ne 4 ]
 then
     echo "4 arguments expected, provided $#. See help!"
@@ -36,79 +38,32 @@ then
     exit
 fi    
 
-model_dir=$(find $1/ -name "model_best.pth.tar")
-if [ "$model_dir" != "$1/model_best.pth.tar" ];
+# Todo: check that $1 ends with extension .pth.tar
+# Todo: check that $2 ends with extension .bw
+
+# Check config files
+config_file=$(ls $3/* | grep "$3/config_params.yaml")
+model_structure=$(ls $3/* | grep -o "$3/model_structure.yaml")
+if [ "$config_file" != "$3/config_params.yaml" ] || [ "$model_structure" != "$3/model_structure.yaml" ]
 then
-    echo "Could not find model_best.pth.tar inside $1!"
-    echo "See help. Note that folder and file names are case sensitive."
-    echo " "
-    print_help
-    exit
-fi
-
-noisy_data=$(ls -d $2/* | grep -o "noisy_data")
-if [ "$noisy_data" != "noisy_data" ]
-then
-    echo "noisy_data folders expected inside $1!"
-    echo "See help. Note that folder names are case sensitive."
-    echo " "
-    print_help
-    exit
-fi
-
-
-noisy_bw=$(ls $2/noisy_data | grep ".bw" | wc -l)
-if [ $noisy_bw -ne 1 ];
-then
-    echo "Only one bigwig file expected inside $2/noisy_data, found $noisy_bw!"
-    echo "See help"
-    echo " "
-    print_help
-    exit
-fi
-
-
-test_interval=$(ls $3/* | grep -o "test_intervals.bed")
-if [ "$training_interval" != "test_intervals.bed" ]
-then
-    echo "*test_intervals.bed expected inside $3!"
+    echo "config_params.yaml and model_structure.yaml files expected inside $3!"
     echo "See help. Note that file names are case sensitive."
     echo " "
     print_help
     exit
 fi
 
+model=$1
+test_atacdata=$2
+config_params=$3/config_params.yaml
+model_structure=$3/model_structure.yaml
+sizes_file=$4
 
-config_file=$(ls $4/* | grep "$4/config_params.yaml")
-model_structure=$(ls $4/* | grep -o "$4/model_structure.yaml")
-if [ "$config_file" != "$4/config_params.yaml" ] || [ "$model_structure" != "$4/model_structure.yaml" ]
-then
-    echo "config_params.yaml and model_structure.yaml files expected inside $4!"
-    echo "See help. Note that file names are case sensitive."
-    echo " "
-    print_help
-    exit
-fi
-
-model="$1/model_best.pth.tar"
-config_params=$4/config_params.yaml
-model_structure=$4/model_structure.yaml
-test_atacdata=$(find $2/noisy_data -name "*.bw")
-test_intervals=$(ls $3/* | grep "test_intervals.bed")
+echo "Make test intervals"
+python $script_dir/get_intervals.py --sizes $sizes_file --intervalsize 50000 --out_dir $out_dir --wg
 
 echo "Read test data over selected intervals and save into .h5 format"
-python $atacworks/bw2h5.py \
-           --noisybw $test_atacdata \
-           --intervals $test_intervals \
-           --prefix test_data \
-           --pad 5000 \
-           --batch_size 2000 \
-           --nolabel
+python $script_dir/bw2h5.py --noisybw $test_atacdata --intervals $out_dir/genome_intervals.bed --prefix test_data --pad 5000 --nolabel
 
 echo "Inference on selected intervals, producing denoised track and binary peak calls"
-python $atacworks/main.py --infer \
-    --infer_files test_data.h5 \
-    --sizes_file $atacworks/example/reference/hg19.auto.sizes \
-    --config $config_params \
-    --config_mparams $model_structure \
-    --infer_threshold 0.5
+python $script_dir/main.py --infer --infer_files $out_dir/test_data.h5 --sizes_file $sizes_file --config $config_params --config_mparams $model_structure
--- scripts/run_training.sh
+++ scripts/run_training.sh
@@ -10,28 +10,36 @@
 # license agreement from NVIDIA CORPORATION is strictly prohibited.
 #
 
-tutorial_dir=$(readlink -f $(dirname "$0"))
-atacworks=$(readlink -f "$tutorial_dir/..")
+script_dir=$(readlink -f $(dirname "$0"))
 
 function print_help {
-    echo "$atacworks/tutorials/run_training.sh <path-to-noisy-and-clean-data-folder> <path-to-intervals-folder> <path-to-configs-folder>"
+    echo "$atacworks/tutorials/run_training.sh <path-to-noisy-clean-data-folder> <path-to-configs-folder> <path-to-chromosome-sizes-file> <chromosome for validation> <chromosome for holdout> <out_dir>"
     echo "<path-to-noisy-clean-data-folder> must contain noisy_data folder, clean_data folder."
     echo "                                  noisy_data folder contains a single bigwig file."
-    echo "                                  clean_data folder contains a bigwig file and a narrowPeak file."
-    echo " "
-    echo "<path-to-intervals-folder>        must contain training_intervals.bed and val_intervals.bed"
+    echo "                                  clean_data folder contains a bigwig file and a narrowPeak or BED file. The first line of the narrowPeak or BED file is expected to be a header."
     echo " "
     echo "<path-to-configs-folder>          must contain config_params.yaml and model_structure.yaml"
+    echo " "
+    echo "<path-to-chromosome-sizes-file>   tab-separated file containing chromosome names and sizes"
+    echo " "
+    echo "<chromosome for validation>       name of chromosome to use for validation"
+    echo " "
+    echo "<chromosome for holdout>          name of chromosome to hold out from training"
+    echo " "
+    echo "<out_dir>                         path to output directory."
+
 }
 
-if [ $# -ne 3 ]
+# Check number of args
+if [ $# -ne 6 ]
 then
-    echo "3 arguments expected, provided only $#. See help!"
+    echo "6 arguments expected, provided only $#. See help!"
     echo " "
     print_help
     exit
 fi    
 
+# Check clean and noisy data
 clean_data=$(ls -d $1/* | grep -o "clean_data")
 noisy_data=$(ls -d $1/* | grep -o "noisy_data")
 if [ "$clean_data" != "clean_data" ] || [ "$noisy_data" != "noisy_data" ]
@@ -64,80 +72,55 @@ then
     print_help
     exit
 fi
-clean_narrowpeak=$(ls $1/clean_data | grep ".narrowPeak" | wc -l)
-echo "clean_narrowpeak: $clean_narrowpeak"
-if [ $clean_narrowpeak -ne 1 ];
+clean_peak=$(ls $1/clean_data | grep ".narrowPeak\|.bed" | wc -l)
+echo "clean_peak: $clean_peak"
+if [ $clean_peak -ne 1 ];
 then
-    echo "Only one bigwig file expected inside $1/clean_data, found $clean_narrowpeak!"
+    echo "Only one peak file expected inside $1/clean_data, found $clean_peak!"
     echo "See help"
     echo " "
     print_help
     exit
 fi
 
-training_interval=$(ls $2/* | grep -o "training_intervals.bed")
-val_interval=$(ls $2/* | grep -o "val_intervals.bed")
-if [ "$training_interval" != "training_intervals.bed" ] || [ "$val_interval" != "val_intervals.bed" ]
+# Check config files
+config_file=$(ls $2/* | grep "$2/config_params.yaml")
+model_structure=$(ls $2/* | grep -o "$2/model_structure.yaml")
+if [ "$config_file" != "$2/config_params.yaml" ] || [ "$model_structure" != "$2/model_structure.yaml" ]
 then
-    echo "training_intervals.bed and val_intervals.bed files expected inside $2!"
+    echo "config_params.yaml and model_structure.yaml files expected inside $2!"
     echo "See help. Note that file names are case sensitive."
     echo " "
     print_help
     exit
 fi
 
-config_file=$(ls $3/* | grep "$3/config_params.yaml")
-model_structure=$(ls $3/* | grep -o "$3/model_structure.yaml")
-if [ "$config_file" != "$3/config_params.yaml" ] || [ "$model_structure" != "$3/model_structure.yaml" ]
-then
-    echo "config_params.yaml and model_structure.yaml files expected inside $3!"
-    echo "See help. Note that file names are case sensitive."
-    echo " "
-    print_help
-    exit
-fi
-
+# Set environment
 echo "Set environment"
 set -e
 
+# Set variables
 noisy_atacdata=$(find $1/noisy_data -name "*.bw")
 clean_atacdata=$(find $1/clean_data -name "*.bw")
-clean_np=$(find $1/clean_data -name "*.narrowPeak")
-training_intervals=$(ls $2/* | grep "training_intervals.bed")
-echo "training_intervl: $training_intervals"
-val_intervals=$(ls $2/* | grep "val_intervals.bed")
-echo "val_intervl: $val_intervals"
-config_params=$3/config_params.yaml
-model_structure=$3/model_structure.yaml
+clean_np=$(find $1/clean_data \(-name "*.narrowPeak" -o -name "*.bed"\))
+config_params=$2/config_params.yaml
+model_structure=$2/model_structure.yaml
+sizes_file=$3
+valchr=$4
+holdoutchr=$5
+out_dir=$6
 
 echo "Convert clean peak file into bigWig format"
-python $atacworks/peak2bw.py $clean_np $atacworks/example/reference/hg19.chrom.sizes --skip 1
+python $script_dir/peak2bw.py --input $clean_np --sizes $sizes_file --out_dir $out_dir --skip 1
+
+echo "Generate train/val/test intervals"
+python $script_dir/get_intervals.py --sizes $sizes_file --intervalsize 50000 --out_dir $out_dir --val $valchr --holdout $holdoutchr
 
 echo "Save the training data and labels into .h5 format"
-python $atacworks/bw2h5.py \
-           --noisybw $noisy_atacdata \
-           --cleanbw $clean_atacdata \
-           --cleanpeakbw $clean_np.bw \
-           --intervals $training_intervals \
-           --prefix train \
-	   --pad 5000 \
-	   --batch_size 2000 \
-           --nonzero
+python $script_dir/bw2h5.py --noisybw $noisy_atacdata --cleanbw $clean_atacdata --cleanpeakbw $out_dir/$clean_np.bw --intervals $out_dir/training_intervals.bed --out_dir $out_dir --prefix train --pad 5000 --nonzero
 
 echo "Save the validation data and labels into .h5 format"
-python $atacworks/bw2h5.py \
-           --noisybw $noisy_atacdata \
-           --cleanbw $clean_atacdata \
-           --cleanpeakbw $clean_np.bw \
-           --intervals $val_intervals \
-           --prefix val \
-	   --pad 5000 \
-	   --batch_size 2000
+python $script_dir/bw2h5.py --noisybw $noisy_atacdata --cleanbw $clean_atacdata --cleanpeakbw $out_dir/$clean_np.bw --intervals $out_dir/val_intervals.bed --out_dir $out_dir --prefix val --pad 5000
 
 echo "Train and validate a model using model parameters saved in the config files"
-python $atacworks/main.py --train \
-        --config $config_params \
-        --config_mparams $model_structure \
-        --train_files train.h5 \
-        --val_files val.h5 \
-        --epochs 5
+python $script_dir/main.py --train --config $config_params --config_mparams $model_structure --train_files $out_dir/train.h5 --val_files $out_dir/val.h5
