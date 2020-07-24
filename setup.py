e4d583bb0c6dcdd32e9210236c36aa25f52dae4f
ntadimeti <ntadimeti@nvidia.com>
2020-07-24 11:58:24 -0400

    Setup version

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ required_packages = \
 
 
 setup(name='atacworks',
-      version='0.2.2',
+      version='0.3.0',
       description='NVIDIA genomics python libraries and utiliites',
       author='NVIDIA Corporation',
       url="https://github.com/clara-genomics/AtacWorks",
